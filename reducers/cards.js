600d7527dd30efe866e60408bd86ef37d35f758b
Alex Hixon <alex@alexhixon.com>
2016-05-11 15:25:02 +1000

    move SRS card scheduling stuff into schedule reducer

--- reducers/cards.js
+++ reducers/cards.js
@@ -1,166 +1,62 @@
-export const STEP_INTERVALS = [
-  60,
-  60*10
-];
+import card from './cards'
 
-// 5 - perfect response
-// 4 - correct response after a hesitation
-// 3 - correct response recalled with serious difficulty
-// 2 - incorrect response; where the correct one seemed easy to recall 
-//     (aka "ah yeah could've gotten that damn it")
-// 1 - incorrect response; the correct one remembered
-//     (aka "oh yeeeeaaaaaaaah, that's right")
-// 0 - complete blackout. (aka "I had no idea")
+const cards = (state = {
+  contentIdToCardId: {},
+  // activityIdToCardId: {},
+  // sentenceIdToCardId: {},
+}, action) => {
+  switch (action) {
+    const cardId = action.payload.id;
 
-export const EASY = 5;
-export const GOOD = 4;
-export const HARD = 3;
-export const NEARLY = 2;
-export const KNEW_BUT_FORGOT = 1;
-export const NO_IDEA = 0; // primarily for "skip"
+    case 'CARD_ADD':
+      return Object.assign({}, state, {
+        [ cardId ]: card (null, action)
+        contentIdToCardId: contentCardMap (state.contentIdToCardId, action.payload)
+      })
+    case 'CARD_SET_DATA':
+      var contentIdToCardId = state.contentIdToCardId;
 
-const updateInterval = (reps, ef) => {
-  if (reps <= 0 || !reps) {
-    return undefined;
-  }
+      const newContentId = action.payload.data.contentId;
+      if (newContentId && state[cardId].data) {
+        // does it need updating?
+        const oldContentId = state[cardId].data.contentId;
+        if (newContentId != oldContentId) {
+          if (oldContentId) {
+            // remove old one, which may not exist
+            contentIdToCardId[oldContentId] = contentIdToCardId[oldContentId].filter (someCardId => someCardId != cardId)
+          }
 
-  switch (reps) {
-    case 1:
-      return 1;
-    case 2:
-      return 4;
-    default:
-      return updateInterval (reps - 1, ef) * ef;
-  }
-}
-
-// valid cards to look at are anything with nextDue <= now or undefined
-// all cards should be cleared of completed status on start of review
-const card = (state = {}, action) => {
-  switch (action.type) {
-    case 'CARD_SCORE':
-      // wake up card
-      var type = state.type == 'new' ? 'learning' : state.type;
-
-      var repetitions = state.repetitions;
-      var ef = state.ef;
-      var interval = state.interval;
-
-      // TODO: create reviewlog; do this as another action?
-      var step = state.step;
-      switch (action.score) {
-        case EASY:
-          // graduate to review
-          step = -1
-          break;
-        case GOOD:
-          // reduce step count
-          step = Math.max (step - 1, -1)
-          break;
-        case HARD:
-          // keep it where it was
-          break;
-        default:
-          // wrong; max out step count
-          step = STEP_INTERVALS.length - 1
-          break;
-      }
-
-      if (type  == 'learning' || type == 'lapsed') {
-        if (step == -1) {
-          // convert to review card
-          type = 'reviewing';
+          // add new one
+          contentIdToCardId[newContentId].push (cardId);
         }
-      } else {
-        // cards that are presently a review card but may need updating
-        if (step >= 0) {
-          // we moved to step > 0, which means we were wrong
-          // convert the card to lapsed
-          type = 'lapsed';
-        } else {
-          // no more steps remain, and we were correct this time
-          // so convert to completed
-          type = 'completed'
-        }
-      }
-
-      // reschedule next due factors and time
-      // only update ef/reps if we *were* a review card
-      if (state.type == 'reviewing') {
-        if (action.score < HARD) {
-          // answer was wrong, reset repetition count
-          repetitions = 0;
-        } else {
-          // answer correct, update ease factor
-          ef = ef + (0.1 - (5 - action.score) * (0.08 + (5 - action.score) * 0.02));
-          ef = ef < 1.3 ? 1.3 : ef; // clamp
-          repetitions++;
-        }
-      }
-
-      // next due is dependent on if we are in a step for
-      // lapsed/learning cards or regular review cards
-      var next_due;
-      if (type != 'completed') {
-        if (step >= 0) {
-          next_due = new Date(action.time);
-          next_due.setSeconds (action.time.getSeconds() + STEP_INTERVALS[step]);
-        } else {
-          // make it due immediately
-          next_due = action.time;
-        }
-      } else {
-        // update interval
-        interval = updateInterval (repetitions, ef);
-
-        next_due = new Date(action.time);
-        next_due.setDate (action.time.getDate() + interval);
       }
 
       return Object.assign({}, state, {
-        type,
-        step,
-        repetitions,
-        nextDue: next_due,
-        interval,
-        seen: state.seen + 1,
-        ef
+        [ cardId ]: card (state[cardId], action),
+        contentIdToCardId
       })
-    default:
-      return state
-  }
-}
+    case 'CARD_SET_TYPE':
+      var contentIdToCardId = state.contentIdToCardId;
 
-const cards = (state = {}, action) => {
-  switch (action.type) {
-    case 'ADD_CARD':
-      var newCard = Object.assign({}, action.card, {
-        nextDue: undefined,
-        repetitions: 0,
-        ef: 2.5,
-        seen: 0,
-        lapses: 0,
-
-        type: 'new', // ('new', 'learning', 'reviewing', 'lapsed', 'completed')
-        step: 0,
-        interval: undefined
-      });
-
-      const id = action.id;
-
-      return Object.assign({}, state, {
-        [ id ]: newCard
-      })
-    case 'SCORE_CARD':
-      if (!state[action.id]) {
-        return state;
+      if (state[cardId].data) {
+        // does it need updating?
+        const oldContentId = state[cardId].data.contentId;
+        if (oldContentId && action.payload.type != state[cardId].type) {
+          if (oldContentId) {
+            // remove old one, which may not exist
+            contentIdToCardId[oldContentId] = contentIdToCardId[oldContentId].filter (someCardId => someCardId != cardId)
+          }
+        }
       }
 
       return Object.assign({}, state, {
-        [ action.id ]: card (state[action.id], action)
+        [ cardId ]: card (state[cardId], action),
+        contentIdToCardId
       })
     default:
-      return state
+      return Object.assign({}, state, {
+        [ cardId ]: card (state[cardId], action)
+      })
   }
 }
 
---
+++ test/reducers/schedule.spec.js
@@ -0,0 +1,131 @@
+import expect from 'expect'
+import schedule, { NEARLY, HARD, GOOD, STEP_INTERVALS } from '../../reducers/schedule'
+
+describe ('schedule', () => {
+  const now = new Date ();
+
+  const exp = new Date (now);
+  exp.setSeconds (now.getSeconds() + STEP_INTERVALS[0]);
+
+  it('should mark an unstarted card as seen and learning', () => {
+    expect(
+      schedule({
+        nextDue: undefined,
+        repetitions: 0,
+        ef: 2.5,
+        seen: 0,
+        lapses: 0,
+
+        type: 'new',
+        step: 0,
+        interval: undefined
+      }, {
+        type: 'SCHEDULE_UPDATE',
+
+        payload: {
+          score: HARD,
+          time: now,
+        }
+      })
+    ).toEqual({
+      nextDue: exp,
+      repetitions: 0,
+      ef: 2.5,
+      seen: 1,
+      lapses: 0,
+
+      type: 'learning',
+      step: 0,
+      interval: undefined
+    })
+  })
+
+  const updated = new Date();
+
+  const justGraduated = {
+    nextDue: updated,
+    repetitions: 0,
+    ef: 2.5,
+    seen: 2,
+    lapses: 0,
+
+    type: 'reviewing',
+    step: -1,
+    interval: undefined
+  };
+
+  it('should graduate a learning card to reviewing', () => {
+    expect(
+      schedule({
+        nextDue: exp,
+        repetitions: 0,
+        ef: 2.5,
+        seen: 1,
+        lapses: 0,
+
+        type: 'learning',
+        step: 0,
+        interval: undefined
+      }, {
+        type: 'SCHEDULE_UPDATE',
+
+        payload: {
+          score: GOOD,
+          time: updated,
+        }
+      })
+    ).toEqual(justGraduated)
+  });
+
+  var finalDue = new Date(updated);
+  finalDue.setDate (finalDue.getDate () + 1); // 10 day
+
+  it('should convert graduated card to completed, with correct nextDue', () => {
+    expect(
+      schedule(justGraduated, {
+        type: 'SCHEDULE_UPDATE',
+
+        payload: {
+          score: GOOD,
+          time: updated,
+        }
+      })
+    ).toEqual({
+      nextDue: finalDue,
+      repetitions: 1,
+      ef: 2.5,
+      seen: 3,
+      lapses: 0,
+
+      type: 'completed',
+      step: -1,
+      interval: 1
+    })
+  })
+
+  var lapsedDue = new Date(updated)
+  lapsedDue.setSeconds (lapsedDue.getSeconds() + STEP_INTERVALS[STEP_INTERVALS.length - 1])
+
+  it('should change graduated card back to lapsed on wrong answer', () => {
+    expect (
+      schedule (justGraduated, {
+        type: 'SCHEDULE_UPDATE',
+        
+        payload: {
+          score: NEARLY,
+          time: updated
+        }
+      })
+    ).toEqual ({
+      nextDue: lapsedDue,
+      repetitions: 0,
+      ef: 2.5,
+      seen: 3,
+      lapses: 1,
+
+      type: 'lapsed',
+      step: 1,
+      interval: undefined
+    })
+  })
+})
